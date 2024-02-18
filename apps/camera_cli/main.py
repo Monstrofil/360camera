dfd59cf93d0b0526bf7b89edd7e0f5739d7581e7
Oleksandr Shyshatskyi <shalal545@gmail.com>
2024-02-18 16:11:40 +0200

    minor changes: remove dead code and move logging to right place
    Signed-off-by: Oleksandr Shyshatskyi <shalal545@gmail.com>

--- apps/camera_cli/main.py
+++ apps/camera_cli/main.py
@@ -8,42 +8,6 @@ from lib.camera.protocol import ServerProtocol
 from lib.rpc.connection.tcp import TcpConnection
 from lib.rpc.executor import Executor
 
-# file: root/__init__.py
-from functools import wraps
-from asyncio import run
-
-
-# This is a standard decorator that takes arguments
-# the same way app.command does but with
-# app as the first parameter
-def async_command(app, *args, **kwargs):
-    def decorator(async_func):
-        # Now we make a function that turns the async
-        # function into a synchronous function.
-        # By wrapping async_func we preserve the
-        # meta characteristics typer needs to create
-        # a good interface, such as the description and
-        # argument type hints
-        @wraps(async_func)
-        def sync_func(*_args, **_kwargs):
-            return run(async_func(*_args, **_kwargs))
-
-        # Now use app.command as normal to register the
-        # synchronous function
-        app.command(*args, **kwargs)(sync_func)
-
-        # We return the async function unmodifed,
-        # so its library functionality is preserved
-        return async_func
-
-    return decorator
-
-
-# as a method injection, app will be replaced as self
-# making the syntax exactly the same as it used to be.
-# put this all in __init__.py and it will be injected into
-# the library project wide
-typer.Typer.async_command = async_command
 
 
 class Application(ServerProtocol):
@@ -55,6 +19,8 @@ class Application(ServerProtocol):
 
     async def run_command(self, name, *args, **kwargs):
         reader, writer = await asyncio.open_connection(host="127.0.0.1", port=8000)
+        logging.info("Connection to the server established")
+
         connection = TcpConnection(reader, writer)
         executor: ServerProtocol = Executor(
             protocol=ServerProtocol, connection=connection
@@ -83,8 +49,6 @@ class Application(ServerProtocol):
 
 
 def main():
-    logging.info("Connnection to the server established")
-
     app = Application()
     app.run()
 
